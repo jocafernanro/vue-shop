08f91b1bb51da64f8d5a208866b5d0995c853f2c
Jose Carlos Fernandez Rodriguez <jocafernanro@gmail.com>
2020-06-17 16:14:41 +0200

    data persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import localForage from "localforage"
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: localForage,
+  modules: ['cart']
+})
+ 
 
 import products from './modules/products'
 import cart from './modules/cart'
@@ -10,5 +18,6 @@ export default new Vuex.Store({
   modules: {
     products,
     cart
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
